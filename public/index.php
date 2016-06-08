3fb8ca429f4bfc8a1a0605a062da497ea634b6c1
Andy Fleming <andy@andyfleming.com>
2016-06-07 21:15:03 -0700

    Cleaning up index and adding service online indicator

--- public/index.php
+++ public/index.php
@@ -12,15 +12,17 @@ if (file_exists('../.env')) {
     $dotenv->load();
 }
 
+if (empty($_FILES)) {
+    die('Service online');
+}
+
 $originalFilename = basename($_FILES['userfile']['name']);
 $originalExtension = (count(explode('.', $originalFilename)) > 1) ? '.' . array_reverse(explode('.', $originalFilename))[0] : '';
 
 $newFilename = uniqid() .  $originalExtension;
 $uploadTarget = 'uploads/' . $newFilename;
 
-echo '<pre>';
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadTarget)) {
     echo "File is valid, and was successfully uploaded.\n";
-} else {
-    echo "Possible file upload attack!\n";
 }
+
