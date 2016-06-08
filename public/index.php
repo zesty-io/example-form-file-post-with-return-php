296751d8399758ae06a4034b82801d0e33caa440
Andy Fleming <andy@andyfleming.com>
2016-06-07 21:24:26 -0700

    Adding redirect back

--- public/index.php
+++ public/index.php
@@ -23,6 +23,9 @@ $newFilename = uniqid() .  $originalExtension;
 $uploadTarget = 'uploads/' . $newFilename;
 
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadTarget)) {
-    echo "File is valid, and was successfully uploaded.\n";
-}
+    //echo "File is valid, and was successfully uploaded.\n";
 
+        $redirectTarget = (!empty($_POST['redirect_target'])) ? $_POST['redirect_target'] : $_SERVER['HTTP_REFERER'];
+        header('Location: ' . $redirectTarget);
+        die();
+}
